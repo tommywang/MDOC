f88406eb1dadc11096ac920403c14fbfb056c1ef
Shiyue Wang <shiyue.wang@me.com>
2012-01-03 06:29:55 +0100

    Un seul DAO marche

---
+++ WebContent/pages/addContactSuccess.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Success</title>
+</head>
+<body>
+	<h1> Contact Ajouté </h1>
+	<h1> Adresse Ajoutée </h1>
+	<h1> Numéro de téléphone n°1 ajouté </h1>
+	<h1> Groupe de contact n°1 ajouté </h1>
+	<input type="submit" value="retour à l'accueil" onclick="javascript:window.location ='accueil.jsp';"/>
+	<p></p>
+</body>
+</html>
--- src/domain/DAOContact.java
+++ src/domain/DAOContact.java
@@ -1,8 +1,8 @@
 package domain;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -12,42 +12,87 @@ import util.HibernateUtil;
 public class DAOContact {
 	
 	private Contact contact;
-	private SessionFactory sessionFactory;
-	private Session session;
-	private Transaction transaction;
+	private Address address;
+	private Set<ContactGroup> contactGroupSet;
+	private Set<PhoneNumber> phoneNumberSet;	
+	
+	public DAOContact(){
+		this.contact = null;
+		this.address = null;
+		this.contactGroupSet = new HashSet<ContactGroup>();
+		this.phoneNumberSet = new HashSet<PhoneNumber>();
+	}
 	
 	public void createContact(String firstName, String lastName, String email){
 		this.contact = new Contact(firstName, lastName, email);
+	}	
+	
+	public void createAdress(String street, String city, String zip, String country){
+		this.address = new Address(street, city, zip, country);
+		this.contact.setAddress(this.address);
+	}
+	
+	public void createContactGroupSet(Set<String> groupNameSet){
+		for (String groupName : groupNameSet){
+			ContactGroup contactGroup = new ContactGroup(groupName);
+			this.contact.addContactGroup(contactGroup);
+			contactGroup.addContact(this.contact);
+			this.contactGroupSet.add(contactGroup);
+		}
+	}
+	
+	public void createPhoneNumberSet(Set<String> phoneNumberSet_, Set<String> phoneKindSet){
+		for (Iterator<String> iterator = phoneKindSet.iterator(), iterator2=phoneNumberSet_.iterator(); iterator.hasNext()&&iterator2.hasNext();) {
+			String phoneNumber_ = (String) iterator2.next();
+			String phoneKind = (String) iterator.next();
+			PhoneNumber phoneNumber = new PhoneNumber(phoneNumber_, phoneKind);
+			this.contact.addPhoneNumber(phoneNumber);
+			phoneNumber.setContact(this.contact);
+			this.phoneNumberSet.add(phoneNumber);
+		}
+	}
+	
+	public void commit(){
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		Transaction transaction = session.beginTransaction();
+		session.save(this.address);
+		for (ContactGroup contactGroup : this.contactGroupSet){
+			session.save(contactGroup);
+		}
+		for (PhoneNumber phoneNumber : this.phoneNumberSet){
+			session.save(phoneNumber);
+		}
+		session.save(this.contact);
+		session.flush();
+		transaction.commit();
+		session.close();
+	}
+	
+	public Address getAddress() {
+		return address;
 	}
 
-	public void createContact(Contact contact){
-		this.contact = contact;
+	public void setAddress(Address address) {
+		this.address = address;
 	}
-	
-	public void addAdress(Address address){
-		this.contact.setAddress(address);
+
+	public Set<ContactGroup> getContactGroupSet() {
+		return contactGroupSet;
 	}
-	
-	public void addPhoneNumber(PhoneNumber phoneNumber){
-		this.contact.addPhoneNumber(phoneNumber);
+
+	public void setContactGroupSet(Set<ContactGroup> contactGroupSet) {
+		this.contactGroupSet = contactGroupSet;
 	}
-	
-	public void addContactGroup(ContactGroup contactGroup){
-		this.contact.addContactGroup(contactGroup);
+
+	public Set<PhoneNumber> getPhoneNumberSet() {
+		return phoneNumberSet;
 	}
-	
-	public void validateContact(){
-		sessionFactory = HibernateUtil.getSessionFactory();
-		session = sessionFactory.openSession();
-		transaction = session.beginTransaction();
-		session.save(this.contact);
+
+	public void setPhoneNumberSet(Set<PhoneNumber> phoneNumberSet) {
+		this.phoneNumberSet = phoneNumberSet;
 	}
-	
-	public void commitContact(){
-		transaction.commit();
-		session.close();	
-	}
-	
+
 	public Contact getContact() {
 		return contact;
 	}
